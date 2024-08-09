6ed9e0c128a63663073bcdeadf53fde9ade5ffc6
Huevos <rowland@moralet.com>
2024-08-09 19:36:14 +0200

    add plugin

---
+++ src/providers.py
@@ -0,0 +1,16 @@
+providers = {
+	"other": {},
+	"uk": {
+		"name": _("UK"),
+		"bouquetname": "FreeView",
+		"sections": {
+			1: _("Entertainment"),
+			100: _("High Definition"),
+			201: _("Children"),
+			230: _("News"),
+			260: _("BBC Interactive"),
+			670: _("Adult"),
+			700: _("Radio"),}},
+	"australia": {
+		"name": _("Australia"),
+		"duplicates": {"lower": 350, "upper": 399}}}
