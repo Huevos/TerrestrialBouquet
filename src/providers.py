c96751a1c437652105e572f48eec70f456595d75
Huevos <rowland@moralet.com>
2024-08-10 11:59:02 +0200

    [Providers] add Italy

--- src/providers.py
+++ src/providers.py
@@ -10,7 +10,12 @@ providers = {
 			230: _("News"),
 			260: _("BBC Interactive"),
 			670: _("Adult"),
-			700: _("Radio"),}},
-	"australia": {
+			700: _("Radio"), }},
+	"it": {
+		"name": _("Italy"),
+		"sections": {
+			1: _("Entertainment"),
+			700: _("Radio"), }},
+	"au": {
 		"name": _("Australia"),
 		"duplicates": {"lower": 350, "upper": 399}}}
